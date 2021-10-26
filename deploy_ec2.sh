61cef7e1151ad14642dff79a2195fb0d9888aafa
Mmota <marcelo.rmota@hotmail.com>
2021-10-26 21:47:17 +0000

    versão final SM

--- deploy_ec2.sh
+++ deploy_ec2.sh
@@ -6,6 +6,6 @@ terraform fmt
 terraform apply -auto-approve
 
 echo [k8s-host] >  ../ansible/hosts
-terraform output |grep Master| sed 's/Master//g'|sed 's/,//g'|sed 's/ //g' >> ../ansible/hosts
+terraform output |grep IP| sed 's/IP//g'|sed 's/,//g'|sed 's/ //g' >> ../ansible/hosts
 
 sleep 10
