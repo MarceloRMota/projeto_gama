53bb3e506aedac6094734119dd44d08a480e9342
Mmota <marcelo.rmota@hotmail.com>
2021-10-27 20:53:44 +0000

    correções no docker build

--- docker_deploy.sh
+++ docker_deploy.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 cd build
-docker login -u=celo -p=M@rcelo123
+export DOCKER_USERNAME="celo"
+export DOCKER_PASSWORD="M@rcelo123"
+docker login -u="${DOCKER_USERNAME}" -p="${DOCKER_PASSWORD}"
 docker build -t celo/projeto_final:0.0.2 -f Dockerfile .
 docker tag celo/projeto_final:0.0.2 hub.docker.com/r/celo/projeto_final
 docker push celo/projeto_final:0.0.2
