312519d8efd6a242a62c5e5e87bb171a7a88cfec
Mmota <marcelo.rmota@hotmail.com>
2021-10-27 20:50:32 +0000

    correções no docker build

--- docker_deploy.sh
+++ docker_deploy.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 cd build
-docker login
+docker login -u=celo -p=M@rcelo123
 docker build -t celo/projeto_final:0.0.2 -f Dockerfile .
 docker tag celo/projeto_final:0.0.2 hub.docker.com/r/celo/projeto_final
 docker push celo/projeto_final:0.0.2
